6de55224ad9f78bb5a9c8b57ab1f5803e37fb050
CHABANI Walid <chabaniw20@gmail.com>
2023-12-20 14:22:16 +0100

    Start Template

---
+++ src/cart_items.js
@@ -0,0 +1,25 @@
+const cartItems =  [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "./Samsung-Galaxy-S7.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "./png-clipart-google-pixel-phone-black-electronics-android-phones-thumbnail.png",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "./png-clipart-xiaomi-mi-note-pro-xiaomi-mi-note-2-xiaomi-redmi-note-4-android-gadget-mobile-phone-thumbnail.png",
+    amount: 1,
+  },
+];
+
+export default cartItems;
